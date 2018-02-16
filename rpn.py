e859812f214bb48d62008ab4e13d6fcc7056bf41
aliceywu <aliceywu@umich.edu>
2018-02-16 11:31:20 -0500

    changes

--- rpn.py
+++ rpn.py
@@ -4,11 +4,23 @@
 # eg 1 1 + is equal to 1 + 1, 1 1 + 2 * is equal to (1+1) * 2
 
 def calculate(arg): 
-    pass
+    stack = list()
 
+    # range-based for loop: setting value for each token
+    for token in arg.split(): 
+        try:
+            value = int(token)
+            stack.append(value)
+
+        # if anything in the try block fails, program jumps to except block
+        except ValueError:
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            return arg1 + arg2
+        
 def main():
     while True:
-        calculate(input('rpn calc> '))
+        print(calculate(input('rpn calc> ')))
 
 if __name__ == '__main__':
     main()
