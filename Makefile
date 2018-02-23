f2788ed6c388e2b9a97373f6030c09548afdab71
aliceywu <aliceywu@umich.edu>
2018-02-23 10:48:08 -0500

    Fixed makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 test:
-    python3 -m unittest
+	# use tab instead of spaces! otherwise, the error is Makefile:2: *** missing separator.  Stop.
+
+	python3 -m unittest
 
 # doesn't exist as an object
 .PHONY: test
--- rpn.py
+++ rpn.py
@@ -1,12 +1,30 @@
-#!/usr/bin/env python333
+#!/usr/bin/env python3
 
 # rpn calculators are easier to use because they have a stack
 # eg 1 1 + is equal to 1 + 1, 1 1 + 2 * is equal to (1+1) * 2
 
-def calculate(arg): 
+# a dictionary is a hash table
+
+# operator is a library of operators
+import operator
+
+
+# create a hash table that maps from a hash table to a function
+# writing a dictionary (key : value)
+operators = {
+    # defining add and subtract like they are functions
+    '+': operator.add,
+    '-': operator.sub,
+}
+
+def calculate(arg):
+    # data type is list: a default constructor
+    # list is an object that has default behavior
+    # lists behave list stacks; can append and pop and push
     stack = list()
 
     # range-based for loop: setting value for each token
+    # arg.split() takes the string and turns it into tokens
     for token in arg.split(): 
         try:
             value = int(token)
@@ -14,20 +32,18 @@ def calculate(arg):
 
         # if anything in the try block fails, program jumps to except block
         except ValueError:
-            if token == '+':
-                arg2 = stack.pop()
-                arg1 = stack.pop()
-                return arg1 + arg2
-                stack.append(result)
-            elif token == '-':
-                arg2 = stack.pop()
-                arg1 = stack.pop()
-                return arg1 - arg2
-                stack.append(result)
-                print(stack)
-            else:
-                print('Not an operator.')
+            # returns the corresponding operator according to the dictionary
+            # definition
+            function = operators[token]
+            # pop these arguments from the stack
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
         print(stack)
+
+    if len(stack) != 1:
+        raise TypeError
     
     # empty the stack and add a return value
     return stack.pop()
